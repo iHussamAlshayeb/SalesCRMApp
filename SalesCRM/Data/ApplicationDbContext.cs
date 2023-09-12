1a3cf8d1d62fbba823d82ad0a82c4f20d4bb64bd
ihussamalshayeb <hussamalshayeb01@gmail.com>
2023-09-12 21:44:03 +0300

    Add project files.

---
+++ SalesCRM/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SalesCRM.Models;
+
+namespace SalesCRM.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<SalesLeadEntity> SalesLead { get; set; }
+    }
+}
